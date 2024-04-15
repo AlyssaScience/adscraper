18402bee6a9640ee6ca18888f1cf7560ea81b5a3
AlyssaScience <alyssa.kalish@gmail.com>
2024-04-15 12:10:58 -0500

    adding specifics

--- crawler/start.sh
+++ crawler/start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 #xvfb-run --server-args="-screen 0 1600x900x24"
-node gen/crawler-cli.js "$@"
+node gen/crawler-cli.js "$@" --crawl_list "./crawlList.txt" --output_dir "./output_dir/" --name "test" --pg_conf_file "./pg_login.json" --scrape_ads --click_ads=clickAndScrapeLandingPage
+# for the click_ads parameter, not sure what we want yet
